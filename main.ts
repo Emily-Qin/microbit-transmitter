10c75ddb0369daf5dc53c1167f2139c30ffd058f
Emily-Qin <68936282+Emily-Qin@users.noreply.github.com>
2020-08-17 00:36:08 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-let morseSend = ""
 let morseLetter: string[] = []
-morseLetter = [""]
+let morseSend = ""
+morseLetter = []
+let letterSend =""
 
 // Button A - adds a dot
 input.onButtonPressed(Button.A, function () {
@@ -26,6 +27,11 @@ input.onButtonPressed(Button.B, function () {
     morseLetter.push("-")
 })
 
+// Shaking - erases the previous entry - > dot/dash
+input.onGesture(Gesture.Shake, function () {
+    morseLetter.pop()
+})
+
 // Button A+B - sends the letter
 input.onButtonPressed(Button.AB, function () {
     // turn off lights first
@@ -34,174 +40,99 @@ input.onButtonPressed(Button.AB, function () {
             led.unplot(i, j)
         }
     }
-    morseSend = ""
-    //unpack the array
+    // unpack the array
     for (let k = 0; k <= morseLetter.length - 1; k++) {
-        morseSend += morseLetter[k]
+        morseSend = "" + morseSend + morseLetter[k]
     }
-    //print
+    // print
     basic.showString(morseSend)
-    radio.sendString(morseSend);
-    //reset array
-    morseLetter= []
+    radio.sendString(morseSend)
+    
+   
+if (morseSend == ".-") {
+        letterSend = "A"
+    } else if (morseSend == "-...") {
+        letterSend ="B"
+    } else if (morseSend == "-.-.") {
+        letterSend = "C"
+    } else if (morseSend == "-..") {
+        letterSend ="D"
+    } else if (morseSend == ".") {
+        letterSend ="E"
+    } else if (morseSend == "..-.") {
+        letterSend ="F"
+    } else if (morseSend == "--.") {
+        letterSend ="G"
+    } else if (morseSend == "....") {
+        letterSend ="H"
+    } else if (morseSend == "..") {
+        letterSend ="I"
+    } else if (morseSend == ".---") {
+        letterSend ="J"
+    } else if (morseSend == "-.-") {
+        letterSend ="K"
+    } else if (morseSend == ".-..") {
+        letterSend ="L"
+    } else if (morseSend == "--") {
+        letterSend ="M"
+    } else if (morseSend == "-.") {
+        letterSend ="N"
+    } else if (morseSend == "---") {
+        letterSend ="O"
+    } else if (morseSend == ".--.") {
+        letterSend ="P"
+    } else if (morseSend == "--.-") {
+        letterSend ="Q"
+    } else if (morseSend == ".-.") {
+        letterSend ="R"
+    } else if (morseSend == "...") {
+        letterSend ="S"
+    } else if (morseSend == "-") {
+        letterSend ="T"
+    } else if (morseSend == "..-") {
+        letterSend ="U"
+    } else if (morseSend == "...-") {
+        letterSend ="V"
+    } else if (morseSend == ".--") {
+        letterSend ="W"
+    } else if (morseSend == "-..-") {
+        letterSend ="X"
+    } else if (morseSend == "-.--") {
+        letterSend ="Y"
+    } else if (morseSend == "--..") {
+        letterSend ="Z"
+    } else if (morseSend == ".----") {
+        letterSend ="1"
+    } else if (morseSend == "..---") {
+        letterSend ="2"
+    } else if (morseSend == "...--") {
+        letterSend ="3"
+    } else if (morseSend == "....-") {
+        letterSend ="4"
+    } else if (morseSend == ".....") {
+        letterSend ="5"
+    } else if (morseSend == "-....") {
+        letterSend ="6"
+    } else if (morseSend == "--...") {
+        letterSend ="7"
+    } else if (morseSend == "---..") {
+        letterSend ="8"
+    } else if (morseSend == "----.") {
+        letterSend ="9"
+    } else {
+        letterSend ="0"
+    } 
+
 })
 
-// Shaking - erases the previous entry - > dot/dash
-input.onGesture(Gesture.Shake, function () {
-    morseLetter.pop()
-})
-
-//RECEIVER
 radio.onReceivedString(function (receivedString) {
-        basic.showString(receivedString);
-
-    //checking for matches
-    //a
-    if (receivedString==".-") {
-        basic.showString("A")
-    }
-    //b
-    else if (receivedString=="-...") {
-        basic.showString("B")
-    }
-    //c
-     else if (receivedString=="-.-.") {
-        basic.showString("c")
-    }
-    //d
-     else if (receivedString=="-..") {
-        basic.showString("D")
-    }
-    //e
-     else if (receivedString==".") {
-        basic.showString("E")
-    }
-    //f
-     else if (receivedString=="..-.") {
-        basic.showString("F")
-    }
-    //g
-     else if (receivedString=="--.") {
-        basic.showString("G")
-    }
-    //h
-     else if (receivedString=="....") {
-        basic.showString("H")
-    }
-    //i
-     else if (receivedString=="..") {
-        basic.showString("I")
-    }
-    //j
-     else if (receivedString==".---") {
-        basic.showString("J")
-    }
-    //k
-     else if (receivedString=="-.-") {
-        basic.showString("K")
-    }
-    //l
-     else if (receivedString==".-..") {
-        basic.showString("L")
-    }
-    //m
-     else if (receivedString=="--") {
-        basic.showString("M")
-    }
-    //n
-     else if (receivedString=="-.") {
-        basic.showString("N")
-    }
-    //o
-     else if (receivedString=="---") {
-        basic.showString("O")
-    }
-    //p
-     else if (receivedString==".--.") {
-        basic.showString("P")
-    }
-    //q
-     else if (receivedString=="--.-") {
-        basic.showString("Q")
-    }
-    //r
-     else if (receivedString==".-.") {
-        basic.showString("R")
-    }
-    //s
-     else if (receivedString=="...") {
-        basic.showString("S")
-    }
-    //t
-     else if (receivedString=="-") {
-        basic.showString("T")
-    }
-    //u
-     else if (receivedString=="..-") {
-        basic.showString("U")
-    }
-    //v
-     else if (receivedString=="...-") {
-        basic.showString("V")
-    }
-    //w
-     else if (receivedString==".--") {
-        basic.showString("W")
-    }
-    //x
-     else if (receivedString=="-..-") {
-        basic.showString("X")
-    }
-    //y
-     else if (receivedString=="-.--") {
-        basic.showString("Y")
-    }
-    //z
-     else if (receivedString=="--..") {
-        basic.showString("Z")
-    }
-    //1
-     else if (receivedString==".----") {
-        basic.showString("1")
-    }
-    //2
-     else if (receivedString=="..---") {
-        basic.showString("2")
-    }
-    //3
-     else if (receivedString=="...--") {
-        basic.showString("3")
-    }
-    //4
-     else if (receivedString=="....-") {
-        basic.showString("4")
-    }
-    //5
-     else if (receivedString==".....") {
-        basic.showString("5")
-    }
-    //6
-     else if (receivedString=="-....") {
-        basic.showString("6")
-    }
-    //7
-     else if (receivedString=="--...") {
-        basic.showString("7")
-    }
-    //8
-     else if (receivedString=="---..") {
-        basic.showString("8")
-    }
-    //9
-     else if (receivedString=="----.") {
-        basic.showString("9")
-    }
-    //0
-    else if (receivedString=="-----") {
-        basic.showString("0")
-    }
-    //anything else
-    else {
-        basic.showString("Invalid")
-    }
-})
+        if (receivedString == letterSend) {
+            basic.showIcon(IconNames.Yes)
+        }
+        else {
+            radio.sendString(morseSend)
+        } 
+        // reset array
+        morseSend = ""
+        morseLetter = []
+    })
